8116e57c9abbe6f3c9c13dc4c532e7b1d534c88d
anonfedora <musaeleazar090@gmail.com>
2024-04-20 16:03:57 +0100

    Setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,18 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
+import { UserService } from './user/user.service';
+import { UserModule } from './user/user.module';
+import { BlogController } from './blog/blog.controller';
+import { BlogService } from './blog/blog.service';
+import { BlogModule } from './blog/blog.module';
+import { AuthService } from './auth/auth.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [MongooseModule.forRoot('mongodb'), 
-    ProductModule],
-  controllers: [AppController],
-  providers: [AppService],
+    ProductModule, UserModule, BlogModule, AuthModule],
+  controllers: [AppController, BlogController],
+  providers: [AppService, UserService, BlogService, AuthService],
 })
 export class AppModule {}
