c5418b03b81176ad5986d86817202522e5fef14c
anonfedora <musaeleazar090@gmail.com>
2024-04-20 15:45:42 +0100

    Setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,30 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import {ConfigModule} from '@nestjs/config';
-import {TypeOrmModule} from '@nestjs/typeorm';
-import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
-import { BlogModule } from './blog/blog.module';
-import { ServeStaticModule } from '@nestjs/serve-static/dist/serve-static.module';
-import { join } from 'path';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, './', 'frontend'),
-    }),
-    ConfigModule.forRoot({isGlobal: true}),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      url: process.env.DATABASE_URL,
-      autoLoadEntities: true,
-      synchronize: true
-    }),
-    UserModule,
-    AuthModule,
-    BlogModule
-  ],
+  imports: [MongooseModule.forRoot('mongodb'), 
+    ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
