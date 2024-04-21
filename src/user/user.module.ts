724ece67c0d8197e31ed0724a47f08301c2a2402
anonfedora <musaeleazar090@gmail.com>
2024-04-21 12:56:23 +0100

    user module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,14 @@
-import { Module } from '@nestjs/common';
-import { UserController } from './user.controller';
+import { Module } from "@nestjs/common";
+import { UserService } from "./user.service";
+import { UserController } from "./user.controller";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UserEntity } from "./models/user.entity";
+import { AuthModule } from "src/auth/auth.module";
 
 @Module({
-  controllers: [UserController]
+    imports: [TypeOrmModule.forFeature([UserEntity]), AuthModule],
+    controllers: [UserController],
+    providers: [UserService],
+    exports: [UserService]
 })
 export class UserModule {}
