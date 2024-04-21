52526646afe5184e32bc125a9f17f5ac7a184ad4
anonfedora <musaeleazar090@gmail.com>
2024-04-21 22:43:57 +0100

    Blog: contrl

--- src/blog/blog.controller.ts
+++ src/blog/blog.controller.ts
@@ -1,4 +1,68 @@
-import { Controller } from '@nestjs/common';
+import {
+    Controller,
+    Post,
+    Body,
+    Request,
+    UseGuards,
+    Get,
+    Query,
+    Param,
+    Delete,
+    Put,
+    UseInterceptors,
+    UploadedFile,
+    Res
+} from "@nestjs/common";
+import { BlogService } from "../service/blog.service";
+import { Observable, of } from "rxjs";
+import { BlogEntry } from "../model/blog-entry.interface";
+import { JwtAuthGuard } from "src/auth/guards/jwt-guard";
+import { BlogEntryEntity } from "../model/blog-entry.entity";
+import { UserIsAuthorGuard } from "../guards/user-is-author.guard";
+import { FileInterceptor } from "@nestjs/platform-express";
+import { diskStorage } from "multer";
+import { v4 as uuidv4 } from "uuid";
+import path = require("path");
+import { Image } from "../model/Image.interface";
+import { join } from "path";
+export const BLOG_ENTRIES_URL = "http://localhost:3000/api/blog-entries";
 
-@Controller('blog')
-export class BlogController {}
+export const storage = {
+    storage: diskStorage({
+        destination: "./uploads/blog-entry-images",
+        filename: (req, file, cb) => {
+            const filename: string =
+                path.parse(file.originalname).name.replace(/\s/g, "") +
+                uuidv4();
+            const extension: string = path.parse(file.originalname).ext;
+
+            cb(null, `${filename}${extension}`);
+        }
+    })
+};
+
+@Controller("blog-entries")
+export class BlogController {
+    constructor(private blogService: BlogService) {}
+
+    @UseGuards(JwtAuthGuard)
+    @Post()
+    create(
+        @Body() blogEntry: BlogEntry,
+        @Request() req
+    ): Observable<BlogEntry> {
+        const user = req.user;
+        return this.blogService.create(user, blogEntry);
+    }
+
+    @Query("")
+    index(@Query("page") page: number = 1, @Query("limit") limit: number = 10) {
+        limit = limit > 100 ? 100 : limit;
+
+        return this.blogService.paginateAll({
+            limit: Number(limit),
+            page: Number(page),
+            route: BLOG_ENTRIES_URL
+        });
+    }
+}
--- src/blog/blog.service.ts
+++ src/blog/blog.service.ts
@@ -21,4 +21,71 @@ export class BlogService {
         private readonly blogRepository: Repository<BlogEntryEntity>,
         private userService: UserService
     ) {}
+
+    create(user: User, blogEntry: BlogEntry): Observable<BlogEntry> {
+        blogEntry.author = user;
+        return this.generateSlug(blogEntry.title).pipe(
+            switchMap((slug: string) => {
+                blogEntry.slug = slug;
+                return from(this.blogRepository.save(blogEntry));
+            })
+        );
+    }
+
+    findAll(): Observable<BlogEntry[]> {
+        return from(this.blogRepository.find({ relations: ["author"] }));
+    }
+
+    paginateAll(
+        options: IPaginationOptions
+    ): Observable<Pagination<BlogEntry>> {
+        return from(
+            paginate<BlogEntry>(this.blogRepository, options, {
+                relations: ["author"]
+            })
+        ).pipe(map((blogEntries: Pagination<BlogEntry>) => blogEntries));
+    }
+
+    paginateByUser(
+        options: IPaginationOptions,
+        userId: number
+    ): Observable<Pagination<BlogEntry>> {
+        return from(
+            paginate<BlogEntry>(this.blogRepository, options, {
+                relations: ["author"],
+                where: [{ author: userId }]
+            })
+        ).pipe(map((blogEntries: Pagination<BlogEntry>) => blogEntries));
+    }
+
+    findOne(id: number): Observable<BlogEntry> {
+        return from(
+            this.blogRepository.findOne({ id }, { relations: ["author"] })
+        );
+    }
+
+    findByUser(userId: number): Observable<BlogEntry[]> {
+        return from(
+            this.blogRepository.find({
+                where: {
+                    author: userId
+                },
+                relations: ["author"]
+            })
+        ).pipe(map((blogEntries: BlogEntry[]) => blogEntries));
+    }
+
+    updateOne(id: number, blogEntry: BlogEntry): Observable<BlogEntry> {
+        return from(this.blogRepository.update(id, blogEntry)).pipe(
+            switchMap(() => this.findOne(id))
+        );
+    }
+
+    deleteOne(id: number): Observable<any> {
+        return from(this.blogRepository.delete(id));
+    }
+
+    generateSlug(title: string): Observable<string> {
+        return of(slugify(title));
+    }
 }
