5cb975d3e6e5bc8040a3d3f8a0883f8cacb23d1a
anonfedora <musaeleazar090@gmail.com>
2024-04-21 13:17:01 +0100

    init user controller, blog

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,49 @@
-import { Controller } from '@nestjs/common';
+import {
+    Controller,
+    Post,
+    Body,
+    Get,
+    Param,
+    Delete,
+    Put,
+    UseGuards,
+    Query,
+    UseInterceptors,
+    UploadedFile,
+    Request,
+    Res
+} from "@nestjs/common";
+import { UserService } from "../user.service";
+import { User, UserRole } from "../models/user.interface";
+import { Observable, of } from "rxjs";
+import { catchError, map, tap } from "rxjs/operators";
+import { hasRoles } from "src/auth/decorators/roles.decorator";
+import { JwtAuthGuard } from "src/auth/guards/jwt-guard";
+import { RolesGuard } from "src/auth/guards/roles.guard";
+import { Pagination } from "nestjs-typeorm-paginate";
+import { FileInterceptor } from "@nestjs/platform-express";
+import { diskStorage } from "multer";
+import { v4 as uuidv4 } from "uuid";
+import path = require("path");
+import { join } from "path";
+import { UserIsUserGuard } from "src/auth/guards/UserIsUser.guard";
 
-@Controller('user')
-export class UserController {}
+export const storage = {
+    storage: diskStorage({
+        destination: './uploads/profileimages',
+        filename: (req, file, cb) => {
+            const filename: string = path.parse(file.originalname).name.replace(/\s/g, '') + uuidv4();
+            const extension: string = path.parse(file.originalname).ext;
+
+            cb(null, `${filename}${extension}`)
+        }
+    })
+}
+
+@Controller("user")
+export class UserController {
+  constructor(private userService: UserService){}
+  
+  @Post()
+  
+}
