7db3f30d037dc880ea64513264d2bd8197c436a1
anonfedora <musaeleazar090@gmail.com>
2024-04-21 10:49:18 +0100

    install

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { UserEntity } from "./models/user.entity";
+import { Repository, Like } from "typeorm";
+import { User, UserRole } from "./models/user.interface";
+import { Observable, from, throwError } from "rxjs";
+import { switchMap, map, catchError } from "rxjs/operators";
+import { AuthService } from "src/auth/auth.service";
+import {
+    paginate,
+    Pagination,
+    IPaginationOptions
+} from "nestjs-typeorm-paginate";
 
 @Injectable()
 export class UserService {}
